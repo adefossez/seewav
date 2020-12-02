d1f9e40c8bd34d266a4a7400f2df4a120cef598f
Alexandre Defossez <alexandre.defossez@gmail.com>
2020-12-02 21:03:05 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Inspired from https://github.com/kennethreitz/setup.py
+
+from pathlib import Path
+
+from setuptools import setup
+
+NAME = 'seewav'
+DESCRIPTION = 'Visualize waveform'
+URL = 'https://github.com/adefossez/seewav'
+EMAIL = 'alexandre.defossez@gmail.com'
+AUTHOR = 'Alexandre Défossez'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = "0.1.0"
+
+HERE = Path(__file__).parent
+
+REQUIRED = [i.strip() for i in open("requirements.txt") if i.strip()]
+
+try:
+    with open(HERE / "README.md", encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=['seewav'],
+    entry_points = {
+        'console_scripts': ['seewav=seewav:main'],
+    },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='Unlicense license',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Video',
+    ],
+)
